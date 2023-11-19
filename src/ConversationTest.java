d2fd94ff74114290be8008bc6a697762a34ea480
Tiago <tiago.ary10@gmail.com>
2023-11-19 10:10:31 -0300

    mudando endpoint de teste

--- src/ConversationTest.java
+++ src/ConversationTest.java
@@ -3,17 +3,49 @@ import java.io.FileWriter;
 import java.util.HashMap;
 import java.io.IOException;
 public class ConversationTest {
+    static String SELECTED_URL = "http://localhost:8080/final_test";
+    static String lineSeparator = System.getProperty("line.separator");
+public static void RunConversationFlow(ConversationStage[] conversationFlow, HashMap<String, String> headers, String csvFileName) {
 
+    try (BufferedWriter bw = new BufferedWriter(new FileWriter(csvFileName + ".csv"))) {
+        // Cabeçalhos do CSV
+        bw.write(String.join(",", "STAGE", "INPUT", "EXPECTED OUTPUT", "ACTUAL OUTPUT", "TEST RESULT"));
+        bw.write(lineSeparator);
 
+        for (ConversationStage stage : conversationFlow) {
+            try {
+                String output = BackendHttpRequest.getBotResponseFromFlask(stage.getInput(), SELECTED_URL, headers);
+                String resultEmoji = output.equals(stage.getExpectedOutput()) ? "✅" : "❌";
+                String truncatedInput = utils.truncateString(stage.getInput());
+                String truncatedExpected = utils.truncateString(stage.getExpectedOutput());
+                String truncatedOutput = utils.truncateString(output);
+
+                // Escrevendo no CSV
+                bw.write(String.join(",",
+                        stage.getStage(),
+                        "\"" + truncatedInput + "\"",
+                        "\"" + truncatedExpected + "\"",
+                        "\"" + truncatedOutput + "\"",
+                        resultEmoji
+                ));
+                bw.write(lineSeparator);
+
+            } catch (Exception e) {
+                System.err.println("Error at stage " + stage.getStage());
+                e.printStackTrace();
+            }
+        }
+    }catch (IOException e) {
+        throw new RuntimeException(e);
+    }
+}
     public static void main(String[] args) {
-        final String SELECTED_URL = "http://localhost:8080/twilioSandbox";
-
         HashMap<String, String> headers = new HashMap<>();
         headers.put("CustomIp", utils.generateRandomIp());
 
 
-        String csvFile = "conversation_log.csv";
-        String lineSeparator = System.getProperty("line.separator");
+        String csvFile = "conversation_log";
+
 
         ConversationStage[] conversationStages = new ConversationStage[]{
                 new ConversationStage("_hello", "Oii", "Parece que você não está cadastrado no nosso sistema, e vou precisar fazer o seu cadastro. Por favor, me informe o seu nome."),
@@ -27,37 +59,7 @@ public class ConversationTest {
                 new ConversationStage("_paymentChoice", "Pix", "Tudo certo então! O pedido estará indo para a sua casa em breve!")
         };
 
-        try (BufferedWriter bw = new BufferedWriter(new FileWriter(csvFile))) {
-            // Cabeçalhos do CSV
-            bw.write(String.join(",", "STAGE", "INPUT", "EXPECTED OUTPUT", "ACTUAL OUTPUT", "TEST RESULT"));
-            bw.write(lineSeparator);
-
-            for (ConversationStage stage : conversationStages) {
-                try {
-                    String output = BackendHttpRequest.getBotResponseFromFlask(stage.getInput(), SELECTED_URL,headers);
-                    String resultEmoji = output.equals(stage.getExpectedOutput()) ? "✅" : "❌";
-                    String truncatedInput = utils.truncateString(stage.getInput());
-                    String truncatedExpected = utils.truncateString(stage.getExpectedOutput());
-                    String truncatedOutput = utils.truncateString(output);
-
-                    // Escrevendo no CSV
-                    bw.write(String.join(",",
-                            stage.getStage(),
-                            "\"" + truncatedInput + "\"",
-                            "\"" + truncatedExpected + "\"",
-                            "\"" + truncatedOutput + "\"",
-                            resultEmoji
-                    ));
-                    bw.write(lineSeparator);
-
-                } catch (Exception e) {
-                    System.err.println("Error at stage " + stage.getStage());
-                    e.printStackTrace();
-                }
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        RunConversationFlow(conversationStages,headers,csvFile);
     }
 
 
